53f6d67ef6ff4b49fe518ec32d4bca3a7ccb7e57
Christopher Brückner <christopher.brueckner95@gmail.com>
2022-06-18 21:28:38 +0200

    test: temporary dummy tests

--- lapspython/utils.py
+++ lapspython/utils.py
@@ -1,7 +1,6 @@
 """Utility functions that do not fit in other modules."""
 
 import dill
-import re
 
 from dreamcoder.dreamcoder import ECResult
 from lapspython.extraction import GrammarParser, ProgramExtractor
@@ -18,29 +17,24 @@ def load_checkpoint(filename: str) -> ECResult:
     with open('checkpoints/' + filename + '.pickle', 'rb') as handle:
         return dill.load(handle)
 
+
 def test_translation_abstraction():
+    """Translate a sample program."""
     result = load_checkpoint('re2_test')
     grammar = GrammarParser(result.grammars[-1]).parsed_grammar
     translator = Translator(grammar)
     hits = ProgramExtractor(result).compact_result.hit_frontiers
-    for hit in hits.values():
+    for _hit in hits.values():
         break
-    program = hit.programs[0]
-    print()
-    print(program)
-    translation = translator.translate(program, hit.name)
-    print()
-    print(translation)
-    e = translation + f"\nprint(re2_train_0_if_the_word_ends_with_any_letter_add_w_after_that('testword'))"
-    exec(e)
+    program = _hit.programs[0]
+    translation = translator.translate(program, _hit.name)
+    return str(translation) + f"\nprint({_hit.name}('testword'))"
+
 
 def test_translation_invented():
+    """Translate a sample invented primitive."""
     result = load_checkpoint('re2_test')
     grammar = GrammarParser(result.grammars[-1]).parsed_grammar
     translator = Translator(grammar)
     invented = result.grammars[-1].productions[0][2]
-    translation = translator.translate(invented)
-    return translation
-
-t = test_translation_invented()
-print(t)
+    return translator.translate(invented, 'f0')
