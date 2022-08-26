f5f3845da913b46fc00eed0c630274877b9b0df4
Christopher Brückner <christopher.brueckner95@gmail.com>
2022-08-26 16:33:52 +0200

    test(stats): test that mock results are 100% solved

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,5 +1,6 @@
 """Unit tests for module lapspython.stats."""
 
+from lapspython.pipeline import Pipeline
 from lapspython.stats import Statistics
 
 
@@ -9,3 +10,16 @@ class TestStatistics:
     def test_init_empty(self):
         """Constructor without result input."""
         assert Statistics().stats == {}
+
+    def test_re2_small(self):
+        """Constructor with mock results."""
+        result = Pipeline.from_checkpoint(
+            're2_test', verbose=False, save=False
+        )
+
+        stats = Statistics().summarize(result)
+        assert stats['programs'] == stats['translations'] == 75
+        assert stats['tasks (total)'] == stats['tasks (solved)'] == 18
+        assert stats['median\t(%)'] == stats['mean\t(%)'] == 100.0
+        assert stats['min\t(%)'] == stats['max\t(%)'] == 100.0
+        assert stats['std\t(%)'] == 0.0
